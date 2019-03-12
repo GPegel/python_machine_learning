f499d86a316937ade7bc78c366ca1be331834f4b
Gerhard Pegel <gpegel@gmail.com>
2019-03-12 22:07:23 +0100

    added a second plot, still buggy

--- training_perceptron_model.py
+++ training_perceptron_model.py
@@ -2,6 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+class Perceptron(object):
+    
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+    
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.errors_ =[]
+        
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+    
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, 1)
+
 # set dictionary
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
 
@@ -18,4 +46,11 @@ plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versico
 plt.xlabel('sepal length in [cm]')
 plt.ylabel('petal length in [cm]')
 plt.legend(loc='upper left')
+plt.show()
+
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of updates')
 plt.show()
